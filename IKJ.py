05f5b00200a51681a0d37abd59b7067b5b6d7f56
hetvi1172 <76486210+hetvi1172@users.noreply.github.com>
2022-04-20 20:28:10 +0530

    Add files via upload

---
+++ IKJ.py
@@ -0,0 +1,28 @@
+import random
+import timeit
+
+startTime=timeit.default_timer()
+n=int(input()) 
+m1=[[None]*n]*n
+m2=[[None]*n]*n
+ans=[] 
+
+for i in range(n):  
+    row=[0]*n 
+    ans.append(row)
+
+for i in range(n):
+    for j in range(n):
+        m1[i][j]=random.randint(1, 100000) 
+        m2[i][j]=random.randint(1, 100000)
+
+for i in range(n): 
+   for k in range(n): 
+      for j in range(n): 
+         ans[i][j]+=m1[i][k]*m2[k][j]
+
+for i in ans: 
+   print(i)
+
+stopTime=timeit.default_timer()
+print('Time:', stopTime-startTime)
---
+++ KIJ.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct timespec start, finish;
+
+int main(){
+    int n;
+    cin>>n;
+
+    int m1[n][n];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            m1[i][j]=rand();
+        }
+    }
+
+    int m2[n][n];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            m2[i][j]=rand();
+        }
+    }
+
+    int ans[n][n];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            ans[i][j]=0;
+        }
+    }
+
+    timespec_get(&start, TIME_UTC);
+    for(int k=0; k<n; k++){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                ans[i][j] += m1[i][k]*m2[k][j];
+            }
+        }
+    }
+    timespec_get(&finish, TIME_UTC);
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cout<<ans[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
+    cout<<finish.tv_sec-start.tv_sec<<" "<<finish.tv_sec-start.tv_nsec;
+
+    return 0;
+}
